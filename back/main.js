94d4ba71907d92450553203cecede55aada02cbd
piment <romain@mura.io>
2022-03-07 13:35:04 +0100

    changed server port

--- back/main.js
+++ back/main.js
@@ -67,7 +67,7 @@ app.get("/", cors(), async (req, res) => {
 
 
 // Start the server on port 3000
-app.listen(3000, function() {
+app.listen(3001, function() {
     console.log("Server started on port 3000");
 });
 
